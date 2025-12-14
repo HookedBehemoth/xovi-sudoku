8f37f16fb54bd1327729043f1b48312d0615727f
HookedBehemoth <luis@scheurenbrand.me>
2025-12-14 16:12:36 +0100

    use xovi codegen for qmldiff call

--- entry.c
+++ entry.c
@@ -5,14 +5,12 @@
 
 void registerQmldiff();
 
-#define qmldiff_add_external_diff ((unsigned long long int (*)(const char*, const char*)) LINKTABLEVALUES[2])
-
 void _xovi_construct() {
     printf("Registering PuzzleManager\n");
     Environment->requireExtension("qt-resource-rebuilder", 0, 2, 0);
     registerQmldiff();
-    qmldiff_add_external_diff(r$toolbar, "Logging of line strokes");
+    qt_resource_rebuilder$qmldiff_add_external_diff(r$toolbar, "Logging of line strokes");
 #ifdef DEBUG
-    qmldiff_add_external_diff(r$strokehook, "Line stroke logging");
+    qt_resource_rebuilder$qmldiff_add_external_diff(r$strokehook, "Line stroke logging");
 #endif
 }
